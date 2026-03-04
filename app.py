43b73df287c3bfc8c7a07ba934d62b869aa0524d
helloaxis <hello.coreaxis@gmail.com>
2026-03-04 18:09:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,3 +10,6 @@ def home():
 @app.route("/health")
 def health():
     return {"status": "running"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
